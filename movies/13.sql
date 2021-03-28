bda1a738102295538a1bd596e18cad37545ba3d6
Tammy Ho <tammyhho98@gmail.com>
2021-03-28 23:20:59 +0000

    Finished movies pset

---
+++ movies/13.sql
@@ -0,0 +1,3 @@
+SELECT name FROM people JOIN stars ON people.id = stars.person_id WHERE stars.movie_id IN (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE people.name = "Kevin Bacon" AND people.birth = 1958) 
+EXCEPT
+SELECT name FROM people WHERE name = "Kevin Bacon";
